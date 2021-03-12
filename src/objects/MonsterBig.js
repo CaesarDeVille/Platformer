f7c97f1873b2e5ca15e5fbe9adcc7f5d1f3e5bc3
Jeune Quentin <CaesarDeVille@users.noreply.github.com>
2021-03-12 04:01:47 +0100

    Par la loi et l'ordre

--- src/objects/MonsterBig.js
+++ src/objects/MonsterBig.js
@@ -15,7 +15,7 @@ class MonsterBig extends ObjetEnnemi{
 
         //on réduit un peu la zone de hit
         this.setBodySize(this.body.width-400,this.body.height-400);
-        this.setOffset(150, 250);
+        this.setOffset(64, 80);
 
         //définir les propriété que l'on va utiliser dans notre animation
 
